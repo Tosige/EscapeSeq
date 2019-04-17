992ea1048336f9168f8fe1ef34143d165fcad887
Tosin Ige <tosin.ige@gmail.com>
2019-04-17 13:22:35 -0400

    initial commit

---
+++ src/com/company/EscapeSeq.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class EscapeSeq {
+
+    public static void main(String[] args) {
+	// Print a box in Java
+        System.out.println("*********");
+        System.out.println("*\t\t*");
+        System.out.println("*\t\t*");
+        System.out.println("*\t\t*");
+        System.out.println("*\t\t*");
+        System.out.println("*********");
+
+        //Insert New Line
+        System.out.println("\n");
+
+        //Print a second identical box
+        System.out.println("*********\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*********");
+
+        //Insert New Line
+        System.out.println("\n");
+
+        //Print an Oval
+        System.out.println("\t***");
+        System.out.println("\t\b*\t*\t");
+        System.out.println("*\t\t*");
+        System.out.println("*\t\t*");
+        System.out.println("\t\b*\t*");
+        System.out.println("\t***");
+    }
+}
